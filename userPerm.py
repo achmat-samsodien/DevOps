64dac612f9e72fdaf86ae1ef162bb05b2403374f
Achmat Samsodien <achmat.samsodien@fuseuniversal.com>
2019-08-08 09:31:57 +0200

    updating test and readme

--- userPerm.py
+++ userPerm.py
@@ -1,22 +1,58 @@
 #!/usr/bin/env python3
 
+
+#Version 1
+# My experience with unittest module is limited, so going with what I know
+# Script not scalable for now but my schedule is bit full and I can't spend more time on this right now
+
+
 import os
 import sys
+import errno
 
+#run with user input
 user = sys.argv[1]
-
+#temp file to test write access
+testFile = 'sampleFile'
 
 if user == "dev1":
-    os.system('sudo -u dev1 /usr/bin/touch /home/dev1/testFile')
-    print(user + " " + "can write to directory ")
-    os.system('sudo -u dev1 /usr/bin/touch /home/dev2/testFile')
+    try:
+        #assume user
+        os.setuid(1002)
+        #write tempFile to user directory
+        with open(os.path.join('/home/dev1/',testFile), 'wb') as temp_file:
+            temp_file.write(buff)
+        print(user + " " + "can write to directory ")
+        #write to another user directory
+        with open(os.path.join('/home/dev2/',testFile), 'wb') as temp_file:
+            temp_file.write(buff)
+        #throw exception if user cannot write to directory
+    except OSError as e:
+        if e.errno == errno.EPERM:
+            print("Error: You do not have permission to write to here")
+        pass
 elif user == "dev2":
-    os.system('sudo -u dev2 /usr/bin/touch /home/dev2/testFile')
-    print(user + " " + "can write to directory ")
-    os.system('sudo -u dev2 /usr/bin/touch /home/dev1/testFile')
+    try:
+        os.setuid(1003)
+        with open(os.path.join('/home/dev1/', testFile), 'wb') as temp_file:
+            temp_file.write(buff)
+        print(user + " " + "can write to directory ")
+        with open(os.path.join('/home/dev2/', testFile), 'wb') as temp_file:
+            temp_file.write(buff)
+    except OSError as e:
+        if e.errno == errno.EPERM:
+            print("Error: You do not have permission to write to here")
+        pass
 elif user == "dev3":
-    os.system('sudo -u dev3 /usr/bin/touch /home/dev3/testFile')
-    print(user + " " + "can write to directory ")
-    os.system('sudo -u dev3 /usr/bin/touch /home/dev1/testFile')
-
+    try:
+        os.setuid(1004)
+        with open(os.path.join('/home/dev1/', testFile), 'wb') as temp_file:
+            temp_file.write(buff)
+        print(user + " " + "can write to directory ")
+        with open(os.path.join('/home/dev2/', testFile), 'wb') as temp_file:
+            temp_file.write(buff)
+    except OSError as e:
+        if e.errno == errno.EPERM:
+            print("Error: You do not have permission to write to here")
+        pass
 exit()
